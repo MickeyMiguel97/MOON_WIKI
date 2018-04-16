67d4f5655fb40d546605c5cb4b11dc8fe975a610
MickeyMiguel97 <00145016@uca.edu.sv>
2018-04-15 23:02:56 -0600

    Cambiando a switch

--- app/src/main/java/com/example/rafaj/fragmentapp/SailorPlanet.java
+++ app/src/main/java/com/example/rafaj/fragmentapp/SailorPlanet.java
@@ -3,7 +3,7 @@ package com.example.rafaj.fragmentapp;
 import java.io.Serializable;
 
 /**
- * Created by maris on 14/4/2018.
+ * Created by Yanira Yanes on 15/04/2018.
  */
 
 public class SailorPlanet implements Serializable {
@@ -16,33 +16,39 @@ public class SailorPlanet implements Serializable {
         this.name = name;
 
         //tomando los recursos de drawable
-        if(pos_imagen==0){
-            img = R.drawable.sun;
+        switch(pos_imagen){
+            case 0:
+                img = R.drawable.sun;
+                break;
+            case 1:
+                img = R.drawable.chibi_moon;
+                break;
+            case 2:
+                img = R.drawable.jupiter;
+                break;
+            case 3:
+                img = R.drawable.saturn;
+                break;
+            case 4:
+                img = R.drawable.sun;
+                break;
+            case 5:
+                img = R.drawable.sun;
+                break;
+            case 6:
+                img = R.drawable.sun;
+                break;
+            case 7:
+                img = R.drawable.sun;
+                break;
+            case 8:
+                img = R.drawable.sun;
+                break;
+            case 9:
+                img = R.drawable.sun;
+                break;
         }
-        else if(pos_imagen==1){
-            img = R.drawable.mercury;
-        }
-        else if(pos_imagen==2){
-            img = R.drawable.venus;
-        }
-        else if(pos_imagen==3){
-            img = R.drawable.chibi_moon;
-        }
-        else if(pos_imagen==4){
-            img = R.drawable.mars;
-        }
-        else if(pos_imagen==5){
-            img = R.drawable.jupiter;
-        }
-        else if(pos_imagen==6){
-            img = R.drawable.saturn;
-        }
-        else if(pos_imagen==7){
-            img = R.drawable.uranus;
-        }
-        else if(pos_imagen==8){
-            img = R.drawable.neptune;
-        }
+
     }
 
     //getters para img
